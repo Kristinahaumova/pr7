fd5936dfe5d791f301dc61da4b0a17ca172c50b9
alexeys6969 <alexshashin.2019@gmail.com>
2025-12-18 12:01:05 +0500

    Получаем ID сессии, логируем сессию. Хаюмова

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -15,6 +15,27 @@
 	
 	if($id != -1) {
 		$_SESSION['user'] = $id;
+		// 1) сщздать сессию
+		// 2) записать события 
+		$ip = $_SERVER["REMOTE_ADDR"];
+		$DateStart = date(format: "Y-m-d H:i:s");
+
+		// Получаем ID сессии
+			$sql = "SELECT `id` FROM `session` WHERE `DateStart` = ?";
+			$stmt = $mysqli->prepare($sql);
+			$stmt->bind_param("s", $DateStart);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$Read = $result->fetch_assoc();
+			$_SESSION["IdSession"] = $Read["id"];
+
+
+
+
+
+			$Sql = "INSERT INTO `logs` (`ip`, `IdUser`, `Date`, `TimeOnline`, `Event`)
+			VALUES ('$ip', '$id', '$DateStart', '00:00:00', 'Пользователь $login авторизовался')";
+	$mysqli->query($Sql);
 	}
 	echo md5(md5($id));
 ?>
